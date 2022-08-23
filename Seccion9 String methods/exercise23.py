54618ca0d60db3ce6ad9b41e9a91b91d2a3c9c8f
Mauricio-Mizhquiri <rolando.mizhquiri@gmail.com>
2022-08-23 13:19:23 -0500

    adding exercises from section 9

---
+++ Seccion9 String methods/exercise23.py
@@ -0,0 +1,10 @@
+'''he following text is given:
+
+text = '100 Days of Code'
+
+Using the appropriate method convert all letters to lowercase. Print the result to the console.
+
+Expected result: 100 days of code
+'''
+text = '100 Days of Code'
+print(text.lower())
