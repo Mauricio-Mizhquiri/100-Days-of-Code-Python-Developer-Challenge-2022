ae97e12896adc67dd6397f2ad80042ef02f9e25d
Mauricio-Mizhquiri <rolando.mizhquiri@gmail.com>
2022-08-23 05:00:31 -0500

    adding exercises from section 3

---
+++ Seccion2 Starter/Exercise0.py
@@ -0,0 +1,7 @@
+#Print the Python version to the console.
+
+    #Expected Result 3.8.10
+
+import sys
+version = sys.version.split()[0]
+print(version)
---
+++ Seccion3 Variables and print function/Exercise1.py
@@ -0,0 +1,15 @@
+''''
+    Exercise 1
+    Create two variables (you can freely choose the names) and assign to them following values:
+
+    'Python'
+
+    '3.8'
+
+    Using these variables and the print() function, print to the console the following text: "I love Python 3.8!"
+'''
+languaje = 'Python'
+version = '3.8!'
+
+print('I love',languaje,version)
+
