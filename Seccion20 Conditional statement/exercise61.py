530cd4362ef566f396bf97c994f282fe0f8eda78
Mauricio-Mizhquiri <rolando.mizhquiri@gmail.com>
2022-08-25 02:22:48 -0500

    adding exercises from section 19

---
+++ Seccion20 Conditional statement/exercise61.py
@@ -0,0 +1,12 @@
+'''The following variable is given:
+number = 1.0
+Test whether the variable is an instance of the built-in class int. Print 'YES' if true, 'NO' if false.
+Expected result: NO'''
+from unicodedata import numeric
+
+
+number = 1.0
+if isinstance(number, int):
+    print('YES')
+else:
+    print('NO')
