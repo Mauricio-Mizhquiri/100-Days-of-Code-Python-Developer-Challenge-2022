e5b25e3170c8021661a40ff64400ec7fd69e1842
Mauricio-Mizhquiri <rolando.mizhquiri@gmail.com>
2022-08-25 14:32:52 -0500

    adding exercises from section 22

---
+++ Seccion22 Conditional statement and foor loop/exercise66.py
@@ -0,0 +1,18 @@
+'''Write a program that checks if the given item:
+
+item = '001'
+
+is in the list:
+
+items = ['001', '000', '003', '005', '006']
+
+If so, remove this item from the list and print this list to the console.
+
+Expected result: ['000', '003', '005', '006']'''
+
+item = '001'
+items = ['001', '000', '003', '005', '006']
+
+if item in items:
+    items.remove('001')
+print(items)
---
+++ Seccion22 Conditional statement and foor loop/exercise667.py
@@ -0,0 +1,9 @@
+'''Write a program that finds all two-digit numbers divisible by 11 (use a for loop). 
+Print the result to the console as comma-separated values as shown below. 
+
+Expected result:  11,22,33,44,55,66,77,88,99
+'''
+result = []
+for valor in range(11,100,11):
+    result.append(str(valor))
+print(','.join(result))
